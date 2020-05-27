d61d4d3c3a545acb842b3bb68d8ccc1d4c6a2703
leowenyang <leowenyang@163.com>
2020-05-27 18:39:51 +0800

    update:

--- example.py
+++ example.py
@@ -1,9 +1,8 @@
 #!/bin/python
 import bluetooth
 
-nearby_devices = bluetooth.discover_devices(lookup_names=True, lookup_class)
+nearby_devices = bluetooth.discover_devices(lookup_names=True)
 print("Found {} devices.".format(len(nearby_devices)))
-print(nearby_devices)
 
 for addr, name in nearby_devices:
   print(" {} - {}".format(addr, name))
