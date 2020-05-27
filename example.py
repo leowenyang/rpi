2e65fcd4d4db4d7e15d73e3596436fb06333f468
leowenyang <leowenyang@163.com>
2020-05-27 18:23:43 +0800

    Add param

--- example.py
+++ example.py
@@ -1,8 +1,9 @@
 #!/bin/python
 import bluetooth
 
-nearby_devices = bluetooth.discover_devices(lookup_names=True)
+nearby_devices = bluetooth.discover_devices(lookup_names=True, lookup_class)
 print("Found {} devices.".format(len(nearby_devices)))
+print(nearby_devices)
 
 for addr, name in nearby_devices:
   print(" {} - {}".format(addr, name))
